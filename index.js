04426d6f6e860be366c0b0cc8e3efb0c49c28aed
BryanGaray <bryangarayacademico@gmail.com>
2023-07-12 01:12:42 -0500

    Visitors server fixed

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
 
+server.use(jsonServer.bodyParser);
 server.use(middlewares);
 server.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
